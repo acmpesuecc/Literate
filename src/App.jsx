2e344af4a194e826095fb58689102773c8a998a7
prithzzz <prithikasoundararajan@gmail.com>
2026-02-21 00:21:45 +0530

    fix: routes update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import Landing from './Landing.jsx'
-import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Landing />} />
-    </Routes>
     </>
   )
 }
