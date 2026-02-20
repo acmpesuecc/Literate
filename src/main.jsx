7d442cbef273609be179893cd4b532762b19b30d
prithzzz <prithikasoundararajan@gmail.com>
2026-02-20 23:02:19 +0530

    fix: fixed client error for gauth login

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const CLIENT_ID = import.meta.env.CLIENT_ID;
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
