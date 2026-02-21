2cbd420b4d33f4fd803007c0190db45b06806384
Boffin-Omega <c.kgagangowda78@gmail.com>
2026-02-21 15:08:20 +0530

    feat: setup redirect after login

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useNavigate } from "react-router";
+
 import { GoogleLogin } from '@react-oauth/google'
 
 const Landing = () => {
+  let navigate = useNavigate();
+
   return (
     <div>
       <GoogleLogin 
-      onSuccess={(credentialResponse) => {console.log(credentialResponse)}} 
+      onSuccess={
+        (credentialResponse) => {
+          console.log(credentialResponse);
+          navigate("/app");
+        }} 
       onError={() => console.log("Failed to login :(")}
       width="200px" theme="outline" />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import Landing from './Landing.jsx';
 const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const router = createBrowserRouter([
-    {path: "/", Component: Landing}
+    {path: "/", Component: Landing},
+    {path: "/app", Component: App}
 ]);
 
 createRoot(document.getElementById('root')).render(
