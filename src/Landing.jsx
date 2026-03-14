27c2104c9baa29e989b71302eacbef02535033e3
annamariacijo-lab <annamariacijo@gmail.com>
2026-03-14 15:30:13 +0530

    commit2_css_Anna_Avantika

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import { useNavigate } from "react-router";
-
+import "./index.css";
 import { GoogleLogin } from '@react-oauth/google'
 
 const Landing = () => {
   let navigate = useNavigate();
 
   return (
-    <div>
-      <GoogleLogin 
-      onSuccess={
-        (credentialResponse) => {
-          console.log(credentialResponse);
-          navigate("/app");
-        }} 
-      onError={() => console.log("Failed to login :(")}
-      width="200px" theme="outline" />
+    <div className="app-container">
+      <aside className="sidebar">
+        <div className="nav-item">About Us</div>
+        <div className="nav-item">The Team</div>
+        <div>
+          <GoogleLogin 
+          onSuccess={
+            (credentialResponse) => {
+              console.log(credentialResponse);
+              navigate("/app");
+            }} 
+          onError={() => console.log("Failed to login :(")}
+          width="200px" theme="outline" />
+        </div>
+      </aside>
+      <main className="main-content">
+        <h1 className="title">Literate</h1>
+      </main>
     </div>
   )
 }
