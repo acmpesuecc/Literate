8454efc25f0eda9a1c48d17e8861d2800dab979a
avantikap07 <avantika.p.0714@gmail.com>
2026-04-03 14:16:21 +0530

    fix/db.js table parameters updated

--- src/db.js
+++ src/db.js
@@ -3,5 +3,6 @@ import { Dexie } from "dexie"
 export const db = new Dexie("IndexedDB")
 db.version(1).stores({
   pdf: "++id, title, pg",
-  highlight:"++id,pg,para,wordlen",
+  highlight:"++id,pdfID,page,block,line,startOffset,endOffset",
+  meta:"++id,recentpdf",
 })
