83646e6e5dd648c0d5827da359887c018a6911a8
avantikap07 <avantika.p.0714@gmail.com>
2026-04-03 17:48:20 +0530

    fix/db.js parameters

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import { Dexie } from "dexie"
 
 export const db = new Dexie("IndexedDB")
 db.version(1).stores({
-  pdfs: "++id, title, pg",
+  pdfs: "++id, currentpdf, title, pg",
   highlights:"++id,pdfID,page,block,line,startOffset,endOffset",
   meta:"++id,recentpdf",
 })
